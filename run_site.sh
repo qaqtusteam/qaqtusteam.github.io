a087340858e17f3795204d5a8fc2b83e523dd8a4
Sagi Shnaidman <sshnaidm@redhat.com>
2018-07-28 19:57:10 +0300

    Scripts

---
+++ run_site.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if ! command -v hugo; then
+	echo "Please install hugo: apt-get install -y hugo / dnf install -y hugo / brew install hugo"
+	exit 1
+fi
+echo "Open in your browser: http://localhost:1313"
+hugo server
